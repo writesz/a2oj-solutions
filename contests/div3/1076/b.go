72e2a8cb80ed90b3cc2b7dbebc73d24a1657a64b
writesz <zhannur.writes@gmail.com>
2026-02-19 14:16:08 +0500

    solved b

---
+++ contests/div3/1076/b.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func solve(in *FastScanner, out *bufio.Writer) {
+	testcases := in.NextInt()
+
+	for t := 0; t < testcases; t++ {
+		n := in.NextInt()
+		p := make([]int, n+1)
+
+		for i := 1; i <= n; i++ {
+			p[i] = in.NextInt()
+		}
+
+		for i := 1; i < n; i++ {
+			currentG := n - i + 1
+
+			if p[i] != currentG {
+				// reverse from i to find greatest
+				j := i + 1
+				for ; j <= n; j++ {
+					if p[j] == currentG {
+						break
+					}
+				}
+
+				for ; i < j; i, j = i+1, j-1 {
+					p[i], p[j] = p[j], p[i]
+				}
+			}
+		}
+
+		for i, v := range p {
+			if i != 0 {
+				fmt.Printf("%d ", v)
+			}
+		}
+		fmt.Println()
+	}
+}
+
+// FastScanner: token-based scanner with low overhead.
+type FastScanner struct {
+	r *bufio.Reader
+}
+
+func NewFastScanner() *FastScanner {
+	return &FastScanner{r: bufio.NewReaderSize(os.Stdin, 1<<20)}
+}
+
+func (fs *FastScanner) NextInt() int {
+	sign, val := 1, 0
+	c, err := fs.r.ReadByte()
+	for (c < '0' || c > '9') && c != '-' {
+		if err != nil {
+			return 0
+		}
+		c, err = fs.r.ReadByte()
+	}
+	if c == '-' {
+		sign = -1
+		c, _ = fs.r.ReadByte()
+	}
+	for c >= '0' && c <= '9' {
+		val = val*10 + int(c-'0')
+		c, err = fs.r.ReadByte()
+		if err != nil {
+			break
+		}
+	}
+	if err == nil {
+		_ = fs.r.UnreadByte()
+	}
+	return sign * val
+}
+
+func (fs *FastScanner) NextInt64() int64 {
+	sign, val := int64(1), int64(0)
+	c, err := fs.r.ReadByte()
+	for (c < '0' || c > '9') && c != '-' {
+		if err != nil {
+			return 0
+		}
+		c, err = fs.r.ReadByte()
+	}
+	if c == '-' {
+		sign = -1
+		c, _ = fs.r.ReadByte()
+	}
+	for c >= '0' && c <= '9' {
+		val = val*10 + int64(c-'0')
+		c, err = fs.r.ReadByte()
+		if err != nil {
+			break
+		}
+	}
+	if err == nil {
+		_ = fs.r.UnreadByte()
+	}
+	return sign * val
+}
+
+func (fs *FastScanner) NextString() string {
+	c, err := fs.r.ReadByte()
+	for c <= ' ' {
+		if err != nil {
+			return ""
+		}
+		c, err = fs.r.ReadByte()
+	}
+	buf := []byte{c}
+	for {
+		c, err = fs.r.ReadByte()
+		if err != nil || c <= ' ' {
+			break
+		}
+		buf = append(buf, c)
+	}
+	return string(buf)
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func GCD(a, b int64) int64 {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func PowMod(base, exp, mod int64) int64 {
+	base %= mod
+	res := int64(1)
+	for exp > 0 {
+		if exp&1 == 1 {
+			res = (res * base) % mod
+		}
+		base = (base * base) % mod
+		exp >>= 1
+	}
+	return res
+}
+
+func LowerBound(a []int, x int) int {
+	return sort.Search(len(a), func(i int) bool { return a[i] >= x })
+}
+
+func UpperBound(a []int, x int) int {
+	return sort.Search(len(a), func(i int) bool { return a[i] > x })
+}
+
+func main() {
+	in := NewFastScanner()
+	out := bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer out.Flush()
+
+	solve(in, out)
+}
