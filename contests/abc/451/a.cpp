6dcd8953499dd4cd680bedf773a0e596d7569494
writesz <zhannur.writes@gmail.com>
2026-03-28 19:22:37 +0500

    solved a

---
+++ contests/abc/451/a.cpp
@@ -0,0 +1,34 @@
+// znnr competitive programming journey
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+using ll  = long long;
+using pii = pair<int,int>;
+using vi  = vector<int>;
+
+void solve() {
+    string s;
+    cin >> s;
+    
+    if (s.size() % 5 == 0) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
